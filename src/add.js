bf34089ae10b16b0e6ca76b41b786013efa495f5
HrushabhPatade <phrushabh13@gmail.com>
2024-07-19 16:47:34 +0530

    handling multiple string numbers using for loop

--- src/add.js
+++ src/add.js
@@ -4,7 +4,18 @@ function add(nums) {
   }
 
   const numArr= nums.split(',');
-  return parseInt(numArr[0],10)+parseInt(numArr[1],10);
+//   return parseInt(numArr[0],10)+parseInt(numArr[1],10);
+for(let i = 0; i<numArr.length; i++){
+    numArr[i] = parseInt(numArr[i], 10);
+}
+
+let sum =0;
+for(let i=0; i<numArr.length; i++){
+    sum = sum + numArr[i];
+}
+
+return sum;
+
 }
 
 module.exports = add;
