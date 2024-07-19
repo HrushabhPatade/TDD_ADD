0fe6236b5c8b2ec278cc89ab922c5c4d24ce288e
HrushabhPatade <phrushabh13@gmail.com>
2024-07-19 19:20:01 +0530

    Test case fails when assert method change of single negative, Refactoring to check function proper working

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ QUnit.test("add", function (assert) {
   assert.equal(add(""), 0);
   assert.equal(add("2,3"), 5);
   assert.equal(add("4,5,3,9"), 21);
-  assert.throws(() => add("1,-3,4"), /-3/);
+  assert.equal(() => add("1,-3,4"), /-3/);
   assert.throws(() => add("-1,-3,-4"), /-1,-3,-4/);
   assert.equal(add("1\n2\n3"), 6);
   assert.equal(add("//;\n1;2"), 3);
