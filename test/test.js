63d78740012af26dc0f8d0c1508b30c193c4968e
HrushabhPatade <phrushabh13@gmail.com>
2024-07-19 18:13:04 +0530

    All test cases working fine

--- test/test.js
+++ test/test.js
@@ -3,12 +3,11 @@ var add = require("../src/add");
 QUnit.module("calculator");
 
 QUnit.test("add", function (assert) {
-  // assert.equal(add(""), 0);
-  // assert.equal(add("2,3"), 5);
-  // assert.equal(add("4,5,3,9"), 21)
-  // assert.throws(()=>add("1,-3,4"), /-3/)
-  // assert.throws(()=>add("-1,-3,-4"), /-1,-3,-4/)
-  // assert.equal(add("1\n2,3"),6);
-//   assert.equal(add("//;\n1;2"), 3);
-    assert.equal(add("//|\n1|2|3"), 6);
+  assert.equal(add(""), 0);
+  assert.equal(add("2,3"), 5);
+  assert.equal(add("4,5,3,9"), 21);
+  assert.throws(() => add("1,-3,4"), /-3/);
+  assert.throws(() => add("-1,-3,-4"), /-1,-3,-4/);
+  assert.equal(add("1\n2,3"), 6);
+  assert.equal(add("//;\n1;2"), 3);
 });
