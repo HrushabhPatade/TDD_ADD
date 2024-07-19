216a3eee17031c143d9d98182d7fc1dd08e98613
HrushabhPatade <phrushabh13@gmail.com>
2024-07-19 17:17:57 +0530

    new line error

--- src/add.js
+++ src/add.js
@@ -3,26 +3,27 @@ function add(nums) {
     return 0;
   }
 
-  const numArr= nums.split(',');
-//   return parseInt(numArr[0],10)+parseInt(numArr[1],10);
-for(let i = 0; i<numArr.length; i++){
+  nums = nums.replace(/\n/g, ",");
+
+  const numArr = nums.split(",");
+  //   return parseInt(numArr[0],10)+parseInt(numArr[1],10);
+  for (let i = 0; i < numArr.length; i++) {
     numArr[i] = parseInt(numArr[i], 10);
-}
+  }
 
-//negative number
-const negativeNums = numArr.filter(i=>i<0);
+  //negative number
+  const negativeNums = numArr.filter((i) => i < 0);
 
-if(negativeNums.length >0){
+  if (negativeNums.length > 0) {
     throw new Error(negativeNums);
-}
+  }
 
-let sum =0;
-for(let i=0; i<numArr.length; i++){
+  let sum = 0;
+  for (let i = 0; i < numArr.length; i++) {
     sum = sum + numArr[i];
-}
+  }
 
-return sum;
-
+  return sum;
 }
 
 module.exports = add;
--- test/test.js
+++ test/test.js
@@ -8,6 +8,7 @@ QUnit.test('add', function(assert){
     // assert.equal(add("2,3"), 5);  
     // assert.equal(add("4,5,3,9"), 21)
     // assert.throws(()=>add("1,-3,4"), /-3/)
-    assert.throws(()=>add("-1,-3,-4"), /-1,-3,-4/)
+    // assert.throws(()=>add("-1,-3,-4"), /-1,-3,-4/)
+    assert.equal(add("1\n2\n3"),6);
 
 })
