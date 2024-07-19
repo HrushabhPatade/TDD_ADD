38d40867dfc9e32ac2f1adfd9f169fa730e584f2
HrushabhPatade <phrushabh13@gmail.com>
2024-07-19 19:23:48 +0530

    adding number to empty string to refactor with expected output

--- test/test.js
+++ test/test.js
@@ -3,9 +3,9 @@ var add = require("../src/add");
 QUnit.module("TDD_ADD");
 
 QUnit.test("add", function (assert) {
-  assert.equal(add(""), 0);
+  assert.equal(add("5"), 0);
   assert.equal(add("2,3"), 5);
-  assert.equal(add("4,5,3,9"), 20);
+  assert.equal(add("4,5,3,9"), 21);
   assert.throws(() => add("1,-3,4"), /-3/);
   assert.throws(() => add("-1,-3,-4"), /-1,-3,-4/);
   assert.equal(add("1\n2\n3"), 6);
