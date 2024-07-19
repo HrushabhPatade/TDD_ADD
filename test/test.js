4835e29638910d9c65751ea533a7f019d8ecc0e6
HrushabhPatade <phrushabh13@gmail.com>
2024-07-19 19:22:09 +0530

    Refactoring output of multiple number string to match add function output

--- test/test.js
+++ test/test.js
@@ -5,8 +5,8 @@ QUnit.module("TDD_ADD");
 QUnit.test("add", function (assert) {
   assert.equal(add(""), 0);
   assert.equal(add("2,3"), 5);
-  assert.equal(add("4,5,3,9"), 21);
-  assert.equal(() => add("1,-3,4"), /-3/);
+  assert.equal(add("4,5,3,9"), 20);
+  assert.throws(() => add("1,-3,4"), /-3/);
   assert.throws(() => add("-1,-3,-4"), /-1,-3,-4/);
   assert.equal(add("1\n2\n3"), 6);
   assert.equal(add("//;\n1;2"), 3);
