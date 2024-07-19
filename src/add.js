d1594e3b0d4673c8864108b7fb2880e79efe1794
HrushabhPatade <phrushabh13@gmail.com>
2024-07-19 16:36:53 +0530

    Single number string test case passed

--- src/add.js
+++ src/add.js
@@ -1,10 +1,10 @@
 function add(nums) {
+  if (nums == "") {
+    return 0;
+  }
 
-    if(nums == ""){
-        return 0;
-    }
-
-
+  const numArr= nums.split(',');
+  return numArr[0]+numArr[1];
 }
 
 module.exports = add;
--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ QUnit.module('calculator');
 
 
 QUnit.test('add', function(assert){
-    assert.equal(add(""), 0);
-   
+    // assert.equal(add(""), 0); 
+    assert.equal(add("2,3"), 5);  
 })
