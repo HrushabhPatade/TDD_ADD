d3035675fd31d06b594057e6bbe5aafc77b16067
HrushabhPatade <phrushabh13@gmail.com>
2024-07-19 17:04:43 +0530

    Negative test case handled for multiple values

--- test/test.js
+++ test/test.js
@@ -7,5 +7,7 @@ QUnit.test('add', function(assert){
     // assert.equal(add(""), 0); 
     // assert.equal(add("2,3"), 5);  
     // assert.equal(add("4,5,3,9"), 21)
-    assert.throws(()=>add("1,-3,4"), /-3/)
+    // assert.throws(()=>add("1,-3,4"), /-3/)
+    assert.throws(()=>add("-1,-3,-4"), /-1,-3,-4/)
+
 })
