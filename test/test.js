7a4e2e059e99b8f3fa2f1bae66be65d74cad4e0e
HrushabhPatade <phrushabh13@gmail.com>
2024-07-19 19:25:03 +0530

    Adding single number string and matching with expected output

--- test/test.js
+++ test/test.js
@@ -3,7 +3,8 @@ var add = require("../src/add");
 QUnit.module("TDD_ADD");
 
 QUnit.test("add", function (assert) {
-  assert.equal(add("5"), 0);
+  assert.equal(add(""), 0);
+  assert.equal(add("5"), 5);
   assert.equal(add("2,3"), 5);
   assert.equal(add("4,5,3,9"), 21);
   assert.throws(() => add("1,-3,4"), /-3/);
